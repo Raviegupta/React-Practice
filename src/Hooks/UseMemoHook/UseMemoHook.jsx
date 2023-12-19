17c205ee2712591e5dc33c343ae8fa1ee659ee97
Raviegupta <raviegupta153@gmail.com>
2023-12-19 18:32:03 +0530

    useMemo Hook PRACTICE

--- src/Hooks/UseMemoHook/UseMemoHook.jsx
+++ src/Hooks/UseMemoHook/UseMemoHook.jsx
@@ -1,23 +1,31 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 const UseMemoHook = () => {
 
     const [counter1, setCounter1] = useState(0);
     const [counter2, setCounter2] = useState(0);
 
-    const isEven = ()=> {
+    const isEven = useMemo(()=> {
         console.warn('isEven Function is called')
         let i = 0;
         while(i<1000000000) i++;
 
         return counter1%2 === 0
-    }
+    }, [counter1])
+
+    // const isEven = ()=> {
+    //     console.warn('isEven Function is called')
+    //     let i = 0;
+    //     while(i<1000000000) i++;
+
+    //     return counter1%2 === 0
+    // }
 
   return (
     <div>
         <h1>UseMemoHook in practice</h1>
 
-        <span>{isEven()? "Even": "Odd"}</span>
+        <span>{isEven? "Even": "Odd"}</span>
         <button onClick={()=> setCounter1(counter1+1)}>Counter1: {counter1}</button>
         <button onClick={()=> setCounter2(counter2+1)}>Counter2: {counter2}</button>
     </div>
